467da87d680b30aabd2d800cb61be971e995bf55
milinkorath <milinkorath@gmail.com>
2015-05-11 12:24:13 +0530

    this is the change from admin side

--- OrgProject/src/org/impel/A.java
+++ OrgProject/src/org/impel/A.java
@@ -3,6 +3,7 @@ package org.impel;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("This is the change from adminside-milinkorath");
 	}
 
 }
